10c6d2af3f1f63d9b43fb6df614e9084e323777d
R.S.Chauhan <srs0447@gmail.com>
2018-10-15 02:21:49 +0530

    Updates

---
+++ js/main.js
@@ -0,0 +1,45 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+
+
+var $el = $(".dates-time");
+function anim() {
+    var st = $el.scrollTop();
+    var sb = $el.prop("scrollHeight") - $el.innerHeight();
+    $el.animate({ scrollTop: st < sb / 2 ? sb : 0 }, 4000, anim);
+}
+function stop() {
+    $el.stop();
+}
+anim();
+$el.hover(stop, anim);
